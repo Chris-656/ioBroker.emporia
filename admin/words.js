f2af05f860e53763b541b8eefdcab7589733c841
Chris-1965 <Besterquester@live.at>
2022-11-30 15:22:20 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"emporia adapter settings": {
+		"en": "Adapter settings for emporia",
+		"de": "Adaptereinstellungen für emporia",
+		"ru": "Настройки адаптера для emporia",
+		"pt": "Configurações do adaptador para emporia",
+		"nl": "Adapterinstellingen voor emporia",
+		"fr": "Paramètres d'adaptateur pour emporia",
+		"it": "Impostazioni dell'adattatore per emporia",
+		"es": "Ajustes del adaptador para emporia",
+		"pl": "Ustawienia adaptera dla emporia",
+		"zh-cn": "emporia的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
