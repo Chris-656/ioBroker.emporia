68b6400beafa8355d9e00ead2490e0735719b88f
Chris-1965 <Besterquester@live.at>
2022-12-07 13:43:28 +0100

    Maint: öanguage staff added

--- admin/words.js
+++ admin/words.js
@@ -16,9 +16,9 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla emporia",
 		"zh-cn": "emporia的适配器设置"
 	},
-	"option1": {
-		"en": "option1",
-		"de": "Option 1",
+	"user": {
+		"en": "Username",
+		"de": "Benutzername",
 		"ru": "Опция 1",
 		"pt": "Opção 1",
 		"nl": "Optie 1",
@@ -28,16 +28,67 @@ systemDictionary = {
 		"pl": "opcja 1",
 		"zh-cn": "选项1"
 	},
-	"option2": {
-		"en": "option2",
-		"de": "Option 2",
-		"ru": "вариант 2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
-	}
+	"password": {
+		"en": "Passwort",
+		"de": "Kennwort",
+		"ru": "Пароль",
+		"pt": "Senha",
+		"nl": "wachtwoord",
+		"fr": "Mot de passe",
+		"it": "Password",
+		"es": "Contraseña",
+		"pl": "Hasło",
+		"zh-cn": "密码"
+	},
+	"refresh": {
+		"en": "Refresh rate",
+		"de": "Aktualisierungsrate",
+		"ru": "Частота обновления",
+		"pt": "Taxa de atualização",
+		"nl": "Vernieuwingsfrequentie",
+		"fr": "Fréquence de rafraîchissement",
+		"it": "Frequenza di aggiornamento",
+		"es": "Frecuencia de actualización",
+		"pl": "Częstotliwość odświeżania",
+		"zh-cn": "刷新率"
+	},
+	"unitoutput": {
+		"en": "Output Units",
+		"de": "Ausgangseinheiten",
+		"ru": "Выходные единицы",
+		"pt": "Unidades de saída",
+		"nl": "Buitenput Units",
+		"fr": "Unités de production",
+		"it": "Unità di uscita",
+		"es": "Dependencias de Productos",
+		"pl": "Jednostka Output",
+		"uk": "Вихідні блоки",
+		"zh-cn": "产出股"
+	},
+	"unitoutputhelper": {
+		"en": "calculates the desired output in Watt or KW",
+		"de": "berechnet die gewünschte Leistung in Watt oder KW",
+		"ru": "вычисляет желаемый выход в Watt или KW",
+		"pt": "calcula a saída desejada em Watt ou KW",
+		"nl": "berekent de verlangde uitput in Watt of KW",
+		"fr": "calcule la puissance souhaitée en Watt ou en KW",
+		"it": "calcola l'output desiderato in Watt o KW",
+		"es": "calcula la salida deseada en Watt o KW",
+		"pl": "oblicza pożądane wyjście w Watt lub KW",
+		"uk": "розраховує бажаний вихід в Ват або КВ",
+		"zh-cn": "计算Watt或KW预期产出"
+	},
+	"refreshhelper": {
+		"en": "In Seconds - Refresh rate for retrieving data from EmpVue device in seconds",
+		"de": "In Seconds - Refresh Rate für das Abrufen von Daten von EmpVue Gerät in Sekunden",
+		"ru": "In Seconds - Рефреш тариф для извлечения данных с устройства EmpVue за считанные секунды",
+		"pt": "Em segundos - Taxa de atualização para recuperar dados do dispositivo EmpVue em segundos",
+		"nl": "Verfrissing voor het vervoeren van gegevens van EmpVue in seconden",
+		"fr": "Dans Secondes - Taux de rechute pour récupérer les données du dispositif EmpVue en quelques secondes",
+		"it": "In Secondi - Frequenza di aggiornamento per il recupero dei dati dal dispositivo EmpVue in pochi secondi",
+		"es": "En Seconds - Tasa de reducción para recuperar datos del dispositivo EmpVue en segundos",
+		"pl": "In Seconds – Refresh szybkość wyszukiwania danych z urządzenia EmpVue w sekundach",
+		"uk": "У секундах - Відновити курс для перерозподілу даних з пристрою EmpVue за секундами",
+		"zh-cn": "在第二批中,从EmpVue装置中检索数据的比率"
+	},
 };
