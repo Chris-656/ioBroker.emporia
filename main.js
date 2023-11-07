ba9da13536f9e526801830ddeca1ef9fe8d208bb
Chris1965 <besterquester@live.at>
2023-11-07 08:12:09 +0100

    Maint

--- main.js
+++ main.js
@@ -416,7 +416,8 @@ class Emporia extends utils.Adapter {
 					this.log.info(`Outlet ${id} changed`);
 					// set plug
 					const res1 = await this.emVue.putEmpOutlet(277738, state.val);
-					this.log.info(`Result ${JSON.stringify(res1)} `);
+					// this.log.info(`Result ${JSON.stringify(res1)} `);
+					this.log.warn(`Switching Outlets is not implemented yet!`);
 					this.setState(id, state.val, true);
 
 				}
